589f748b643f46d8f5345509fdb7d7ab5665e714
Yukio <yukio5347@gmail.com>
2023-05-09 13:08:29 +0900

    add networks property to hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.WALLET_PRIVATE_KEY || ''],
+    },
+  },
 };
 
 export default config;
